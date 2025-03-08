851fa4df0e6d34038ed50253dec26ede0ec41f15
Harish <harish@example.com>
2025-03-08 20:20:14 +0000

    Initial commit for day-1

---
+++ ec2_creation.yaml/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0cb91c7de36eed2cb"
+    instance_type = "t2.micro"
+    key_name      = "key1"
+    tags = {
+        name = "ExampleInstance"
+    }
+}
